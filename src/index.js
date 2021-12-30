b333b4c17ad638f7e8bbba9e1c9ad3cf6704ad75
Casey Carroll <caseydean8@gmail.com>
2021-12-30 11:14:38 -0500

    Add "active" styling in invoices with NavLink.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Expenses from "./routes/expenses";
 import Invoices from "./routes/invoices";
-import Invoice from './routes/invoice'
+import Invoice from "./routes/invoice";
 
 const rootElement = document.getElementById("root");
 
@@ -13,13 +13,17 @@ render(
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />}>
-          <Route index element={
-            <main style={{ padding: "1rem"}}>
-              <p>Select an Invoice</p>
-            </main>
-          } />
+          {/* Index route below doesn't have its own path, it shares a path with its parent. Index route is the default child for the parent route. It renders when the user hasn't clicked an item in the invoice navigation list yet.*/}
+          <Route
+            index
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>Select an Invoice</p>
+              </main>
+            }
+          />
           <Route path=":invoiceId" element={<Invoice />} />
-          </Route>
+        </Route>
         <Route
           path="*"
           // "*" matches when no other routes do.
