f4463ff1de3bd4c7cc0ab74e72f8eeb6cc53f610
Casey Carroll <caseydean8@gmail.com>
2021-12-30 10:58:26 -0500

    Added an index route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Expenses from "./routes/expenses";
 import Invoices from "./routes/invoices";
+import Invoice from './routes/invoice'
 
 const rootElement = document.getElementById("root");
 
@@ -11,7 +12,23 @@ render(
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route index element={
+            <main style={{ padding: "1rem"}}>
+              <p>Select an Invoice</p>
+            </main>
+          } />
+          <Route path=":invoiceId" element={<Invoice />} />
+          </Route>
+        <Route
+          path="*"
+          // "*" matches when no other routes do.
+          element={
+            <main style={{ padding: "1rem" }}>
+              <h3>There's nothing here!</h3>
+            </main>
+          }
+        />
       </Route>
     </Routes>
   </BrowserRouter>,
